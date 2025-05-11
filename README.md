b2b0bba4e85c981b31a7f635e7c27b0032017f22
sandya-maker <sandeshharne502@gmail.com>
2025-05-11 12:56:16 +0530

    All files

---
+++ README.md
@@ -0,0 +1,10 @@
+# Java programs
+
+- Core java
+- OOPs
+- Collection framework
+- Multi threading
+- Design pattern
+- File IO
+- Other miss concept
+- Exception
---
+++ src/ex_06072024/Lab002.java
@@ -0,0 +1,9 @@
+package ex_06072024;
+
+public class Lab002 {
+    public static void main(String[] args) {
+        System.out.println(2+2);
+        System.out.println(2/2);
+        System.out.println(2*2);
+    }
+}
