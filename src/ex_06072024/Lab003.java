a79c4bbe2be9394ef23653fa21813b9f37e5d9bc
sandya-maker <sandeshharne502@gmail.com>
2025-05-11 13:47:37 +0530

    All files

--- src/ex_06072024/Lab003.java
+++ src/ex_06072024/Lab003.java
@@ -2,6 +2,6 @@ package ex_06072024;
 
 public class Lab003 {
     public static void main(String[] args) {
-        System.out.println("sandesh harne");
+        System.out.println("sandeshharne");
     }
 }
